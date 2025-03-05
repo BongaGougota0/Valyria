001ee53e1ffccd9a7f7c3a8e568273fd35705c40
BongaGougota0 <bonga_gougota@outlook.com>
2025-03-05 22:08:16 +0200

    ApplicationContext in this case is the entry point of the programm. If all code is structured well, the required bean DataService should be return with object data created. The data is made possible by implementing automatic method invocation by decorating one the beans method with @PostConstruct just like in SpringBoot.

--- ModelDependencyInjector/src/Main.java
+++ ModelDependencyInjector/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.List;
+
+import beans.DataService;
+import config.ApplicationContext;
+import config.ProjectConfig;
+import model.Orders;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		ApplicationContext app = new ApplicationContext(ProjectConfig.class);
+		DataService dataService = app.getBean(DataService.class);
+		List<Orders> orders = dataService.getAllOrders();
+		for(Orders order : orders) {
+			System.out.println(String.format("Order no. %s with comments : %s", order.orderNumber, order.comments));
+		}
 	}
-
 }
