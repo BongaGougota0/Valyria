c88645a4d8dafaf18dfe9b67b2cf31250d5b0bf1
BongaGougota0 <bonga_gougota@outlook.com>
2026-04-12 11:47:41 +0200

    some intellij and springproject thingies.

---
+++ ChatImplWithRedis/src/test/java/za/co/chatimplwithredis/ChatImplWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.chatimplwithredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatImplWithRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
